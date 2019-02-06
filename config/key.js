84069fa41e440d198ef7cc2d2ce52c81f03df94f
KarthickCSK <karthick.c59@wipro.com>
2019-02-06 19:39:53 +0530

    changes

--- config/key.js
+++ config/key.js
@@ -9,6 +9,12 @@ const blobkeys = {
 };
 
 module.exports = {
-  ...blobkeys,
+  fnUri: "https://dpl-uc3.azurewebsites.net/api/uc3-file-fn",
+  blobAccountName: "wrdmstorageamit",
+  blobAccountKey:
+    "Em5S2KQCBfqiBhwgVGig1PgPNb4Gt3Cv8wbcsFhRBBWIjE2vkFyTSF6RYgvEyH5ikA026Hv3hya1xH8d9xAiXA==",
+  connectionStr:
+    "DefaultEndpointsProtocol=https;AccountName=wrdmstorageamit;AccountKey=Em5S2KQCBfqiBhwgVGig1PgPNb4Gt3Cv8wbcsFhRBBWIjE2vkFyTSF6RYgvEyH5ikA026Hv3hya1xH8d9xAiXA==;EndpointSuffix=core.windows.net",
+  containerName:'firmware',
   port: process.env.PORT || 3008
 };
